de1feabd77075560cc45cd6c913fb8d3c9e826b6
dsod <d.soderling@live.se>
2020-07-31 01:28:47 +0200

    Completed the layout of the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid bg-gradient-dark">
+        <div className="row vh-100 align-items-center justify-content-center">
+          <div
+            className="card border-dark col-10 p-4"
+            style={{ maxWidth: '500px' }}
+            id="quote-box"
+          >
+            <div className="card-body">
+              <h2 id="text">
+                "
+                <small className="text-muted">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </small>
+                "
+              </h2>
+              <p className="lead text-right" id="author">
+                Author
+              </p>
+              <div className="d-flex justify-content-between align-items-center">
+                <a
+                  href="https://twitter.com/intent/tweet"
+                  id="tweet-quote"
+                  title="Post quote on twitter!"
+                >
+                  <i className="fab fa-twitter fa-2x"></i>
+                </a>
+                <button
+                  className="btn btn-link p-0"
+                  type="button"
+                  id="new-quote"
+                  title="Next quote"
+                >
+                  <i class="fas fa-angle-double-right fa-2x"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
